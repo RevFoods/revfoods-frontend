5a9e537dda4b3631de195f0d69a6ec17a866b7e9
Akshay Sargar <akshay.sargar17@vit.edu>
2021-10-07 07:49:24 +0530

    Update order.service.ts

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -7,25 +7,31 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class OrderService {
+  
   url: string;
+  
   constructor(private http: HttpClient) {
-    this.url = "http://localhost:2020";
+    this.url = "http://localhost:2020/order";
   }
-  public addOrder( order:Order) : Observable<Order>{
-    return this.http.post<Order>(this.url + "",order);
+  
+  public addOrder(order: Order): Observable<Order> {
+    return this.http.post<Order>(this.url, order);
   }
-  public updateorder(order :Order): Observable<Order> {
-    return this.http.put<Order>(this.url + "/updateorder", order);
+  
+  public updateorder(order: Order): Observable<Order> {
+    return this.http.put<Order>(this.url, order);
   }
-  public deleteOrder(orderId: number){
-    return this.http.delete(this.url + "/deleteOrder"+orderId);
+  
+  public deleteOrder(orderId: number) {
+    return this.http.delete(this.url + "/" + orderId);
   }
-  public getOrderById(orderId: number) :Observable<Order> {
-    return this.http.get<Order>(this.url + "/getOrderById"+orderId);
+  
+  public getOrderById(orderId: number): Observable<Order> {
+    return this.http.get<Order>(this.url + "/" + orderId);
   }
 
-  public getAllOrders(): Observable<Order[]>{
-    return this.http.get<Order[]>(this.url+"/getallOrders")
+  public getAllOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.url+"s")
   }
   
 }
