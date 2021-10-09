c2a001181dc64d348a406b6c4d69e3f3d3bc6b0c
“Akshay <akshay.sargar17@vit.edu>
2021-10-09 13:04:29 +0530

    Modified Food

---
+++ src/app/service/food.service.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from "@angular/core/testing";
+import { FoodService } from "./food.service";
+
+describe("FoodCategoryService", () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it("should be created", () => {
+    const service: FoodService = TestBed.get(FoodService);
+    expect(service).toBeTruthy();
+  });
+});
