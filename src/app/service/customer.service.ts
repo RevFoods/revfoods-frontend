b1cd8c6768a911ffd4691491c91f4907a9a3cf62
Hari Prasanth <hariprasanth919@gmail.com>
2021-10-10 17:46:29 +0530

    TASK-41 Added Customer Custom Methods

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Customer } from "../model/customer";
+import { Feedback } from "../model/feedback";
+import { Help } from "../model/help";
 
 @Injectable({
   providedIn: "root",
@@ -31,5 +33,40 @@ export class CustomerService {
 
   public deleteCustomer(customerId: number) {
     return this.http.delete(this.url + "/" + customerId);
+  }
+
+  public addCustomerTableToCustomer(
+    customerTableId: number,
+    customerId: number
+  ): Observable<Customer> {
+    return this.http.put<Customer>(
+      this.url + "/" + customerId + "/customerTable/" + customerTableId,
+      customerTableId
+    );
+  }
+
+  public getAllCustomersByOrderStatusId(
+    orderStatusId: number
+  ): Observable<Customer[]> {
+    return this.http.get<Customer[]>(
+      this.url + "s/orderStatus/" + orderStatusId
+    );
+  }
+
+  public addFeedbackToCustomer(
+    customerId: number,
+    feedback: Feedback
+  ): Observable<Customer> {
+    return this.http.put<Customer>(
+      this.url + "/" + customerId + "/feedback",
+      feedback
+    );
+  }
+
+  public addHelpToCustomer(
+    customerId: number,
+    help: Help
+  ): Observable<Customer> {
+    return this.http.put<Customer>(this.url + "/" + customerId + "/help", help);
   }
 }
